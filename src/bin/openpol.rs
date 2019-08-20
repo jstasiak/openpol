f01ddf27e0774a116d13184b10d276bd30dce915
Jakub Stasiak <jakub@stasiak.at>
2019-08-21 00:45:56 +0200

    Display a piece of intro in a rudimentary fashion
    The S00*.DAT files are FLIC videos. Thanks to a great libflic Rust
    library[1] by David Wang and its great SDL2-based FLIC player example[2]
    setting this up was super easy.
    
    Yes, the playback speed is wrong, it'll be dealt with.
    
    [1] https://github.com/wangds/libflic
    [2] https://github.com/wangds/libflic/blob/64d2927e430cd905d77e1a05bb565dbeaf63ea7a/examples/quickfli.rs

--- src/bin/openpol.rs
+++ src/bin/openpol.rs
@@ -1,6 +1,10 @@
+use flic::{FlicFile, RasterMut};
+use openpol::image13h::indices_to_rgb;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
-use sdl2::pixels::Color;
+use sdl2::pixels::{Color, PixelFormatEnum};
+use std::env;
+use std::path;
 
 const VERSION: &'static str = env!("GIT_DESCRIPTION");
 
@@ -20,6 +24,28 @@ fn main() -> Result<(), String> {
 
     canvas.set_draw_color(Color::RGB(0, 0, 0));
     let mut event_pump = sdl.event_pump()?;
+
+    let args: Vec<String> = env::args().skip(1).collect();
+    if args.len() != 1 {
+        return Err("Usage: openpol GAMEDIR".to_string());
+    }
+    let root_dir = path::Path::new(&args[0]);
+    let data_dir = root_dir.join("data");
+    let mut flic = FlicFile::open(&data_dir.join("S002.DAT")).map_err(|e| e.to_string())?;
+    let flic_width = flic.width() as usize;
+    let flic_height = flic.height() as usize;
+    let mut flic_buffer = vec![0; flic_width * flic_height];
+    let mut flic_palette = vec![0; 3 * 256];
+
+    let texture_creator = canvas.texture_creator();
+    let mut texture = texture_creator
+        .create_texture_streaming(
+            PixelFormatEnum::RGB24,
+            flic_width as u32,
+            flic_height as u32,
+        )
+        .map_err(|e| e.to_string())?;
+
     'running: loop {
         // get the inputs here
         for event in event_pump.poll_iter() {
@@ -32,7 +58,19 @@ fn main() -> Result<(), String> {
                 _ => (),
             }
         }
+        let mut raster =
+            RasterMut::new(flic_width, flic_height, &mut flic_buffer, &mut flic_palette);
+        // TODO: Make the actual playback speed match the desired playback speed
+        flic.read_next_frame(&mut raster)
+            .map_err(|e| e.to_string())?;
+        texture.with_lock(None, |buffer: &mut [u8], _pitch: usize| {
+            // NOTE: pitch is assumed to be equal to video width * 3 bytes (RGB), eg. there are no
+            // holes between rows in the buffer.
+            indices_to_rgb(&flic_buffer, &flic_palette, buffer)
+        })?;
+
         canvas.clear();
+        canvas.copy(&texture, None, None)?;
         canvas.present();
     }
 
