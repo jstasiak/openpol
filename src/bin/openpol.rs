7730b067c39ba9afd31d2620ff00bae5df14ac10
Jakub Stasiak <jakub@stasiak.at>
2019-08-20 23:35:36 +0200

    Create an openpol binary with an empty SDL window
    Humble beginnings.

---
+++ src/bin/openpol.rs
@@ -0,0 +1,38 @@
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
+
+fn main() -> Result<(), String> {
+    let sdl = sdl2::init()?;
+    let video = sdl.video()?;
+    let window = video
+        .window("openpol", 320, 200)
+        .build()
+        .map_err(|e| e.to_string())?;
+    let mut canvas = window
+        .into_canvas()
+        .target_texture()
+        .present_vsync()
+        .build()
+        .map_err(|e| e.to_string())?;
+
+    canvas.set_draw_color(Color::RGB(0, 0, 0));
+    let mut event_pump = sdl.event_pump()?;
+    'running: loop {
+        // get the inputs here
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => break 'running,
+                _ => (),
+            }
+        }
+        canvas.clear();
+        canvas.present();
+    }
+
+    Ok(())
+}
