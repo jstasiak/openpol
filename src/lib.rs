8a152167a2c9bd0f884dc4f12e25c4317138879d
Jakub Stasiak <jakub@stasiak.at>
2019-08-23 01:24:10 +0200

    Implement converting raw RGB data to self-contained PPM
    This will be useful soon.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 pub mod fontdat;
 pub mod image13h;
 pub mod paldat;
+pub mod ppm;
 pub mod sounddat;
---
+++ src/ppm.rs
@@ -0,0 +1,44 @@
+use std::io;
+
+/// Write an image stored in RGB values in `data` to a writer using PPM text format. `data` is
+/// interpreted as `width * height` 3-byte RGB values stored row by row.
+pub fn write_ppm<T: io::Write>(
+    width: usize,
+    height: usize,
+    data: &[u8],
+    mut w: T,
+) -> io::Result<()> {
+    write!(w, "P3\n{} {}\n255\n", width, height)?;
+
+    for y in 0..height {
+        for x in 0..width {
+            let offset = 3 * (y * width + x);
+            let color = &data[offset..offset + 3];
+            write!(w, "{} {} {} ", color[0], color[1], color[2])?;
+        }
+        write!(w, "\n")?;
+    }
+    w.flush()?;
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::ppm::write_ppm;
+    use std::str;
+
+    #[test]
+    fn test_write_ppm_works() {
+        let data = [255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 255, 255, 255];
+        let mut buffer = Vec::new();
+        write_ppm(3, 2, &data, &mut buffer).unwrap();
+        let got = str::from_utf8(&buffer).unwrap();
+        let expected = "P3
+3 2
+255
+255 0 0 0 0 0 0 0 0 
+0 0 0 0 0 0 255 255 255 
+";
+        assert_eq!(got, expected);
+    }
+}
